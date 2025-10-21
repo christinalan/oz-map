bb60e761e13183bf0720c7a636a06736c3c51741
Jeff Ong <jonger4@gmail.com>
2025-10-21 16:24:00 -0400

    Try static route pregeneration for /explore/*

--- app/explore/[hotspotId]/default.tsx
+++ app/explore/[hotspotId]/default.tsx
@@ -1,10 +1,17 @@
-"use client";
+import { hotspots } from "../../data/hotspots";
 import HotspotModal from "../@modal/[hotspotId]/page";
+
+// Generate static params for all hotspot routes
+export async function generateStaticParams() {
+	return hotspots.map((hotspot) => ({
+		hotspotId: hotspot.id,
+	}));
+}
 
 export default function Default({
 	params,
 }: {
 	params: Promise<{ hotspotId: string }>;
 }) {
-	return <HotspotModal params={params} />
+	return <HotspotModal params={params} />;
 }
